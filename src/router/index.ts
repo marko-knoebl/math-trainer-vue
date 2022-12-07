a5305e4c472b44284e1446ce1dc79ef9457f0819
Marko Knöbl <marko.kn@gmail.com>
2022-12-07 14:30:07 +0100

    web hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import BasicArithmetic from "../basic-arithmetic/BasicArithmetic.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../home/HomeView.vue";
 import TimesTableBingo from "../times-table-bingo/TimesTableBingo.vue";
 import EquationsNegative from "../equations-negative/EquationsNegative.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
