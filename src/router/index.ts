7bd97583efec997fcd63b6c4ed067275c2a84a07
Marko Knöbl <marko.kn@gmail.com>
2022-12-07 13:54:26 +0100

    build for deployment

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,14 +19,6 @@ const router = createRouter({
     },
     { path: "/equations-negative", name: "", component: EquationsNegative },
     { path: "/basic-arithmetic", name: "", component: BasicArithmetic },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
   ],
 });
 
